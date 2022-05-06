1ebb2a1ad0ec5c5970e0c8ae97324fbce6501ee5
USER <USER@192.168.3.77>
2022-05-06 14:49:58 +0900

    add a file

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/in")
+	public String formby(Model model) {
+	model.addAttribute("user" ,new User());
+	return "register";
+	
+	
+}
+	@PostMapping("/doregister")
+	public String registerby(@Valid  User user, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return"register";
+		}
+		return "loging";
+		
+	}
+}
