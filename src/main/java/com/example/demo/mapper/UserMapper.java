8fb32708d0983ff53ec474c16d3eb7bd317fd9ef
USER <USER@192.168.3.77>
2022-05-06 15:25:14 +0900

    add a database system

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.model.User;
+
 @Mapper
 public interface UserMapper {
-
+void doinsert(User user);
 }
