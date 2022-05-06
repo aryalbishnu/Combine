d9815bd47c0645ecbb7f265f58bd408746d4e56a
USER <USER@192.168.3.77>
2022-05-06 13:54:57 +0900

    add a User java

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+private int id;
+private String username;
+private String email;
+private String password;
+private String city;
+}
