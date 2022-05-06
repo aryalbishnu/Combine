73fc795247287e7d4fbb6ff6f1255a1a7826c6e3
aryalbishnu <ariyalbishnu@gmail.com>
2022-05-06 16:04:57 +0900

    change demo

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,17 +2,20 @@ package com.example.demo.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.User;
 
 @Controller
 public class DemoController {
-
+@Autowired
+UserMapper userMapper;
 	@GetMapping("/in")
 	public String formby(Model model) {
 	model.addAttribute("user" ,new User());
@@ -25,6 +28,7 @@ public class DemoController {
 		if(result.hasErrors()) {
 			return"register";
 		}
+		userMapper.doinsert(user);
 		return "loging";
 		
 	}
